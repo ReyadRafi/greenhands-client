e73207d7d47789801ddfcf1d3939603b4ff42e4b
Reyad Rafi <reyadrafi009v2@gmail.com>
2026-09-10 12:58:09 +0600

    Attach Firebase ID token to all API requests via axios interceptor

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,8 +1,24 @@
 import axios from "axios";
+import { getAuth } from "firebase/auth";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5000",
 });
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const auth = getAuth();
+    const currentUser = auth.currentUser;
+
+    if (currentUser) {
+      const token = await currentUser.getIdToken();
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 const useAxios = () => {
   return axiosInstance;
